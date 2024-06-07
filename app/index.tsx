c143ea7b56360867764bd36647aaa6f5d2c51cb8
hadi-razal <hadhirasal22@gmail.com>
2024-06-07 15:58:27 +0530

    checking fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,27 @@
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
-
+import { StyleSheet, View, Text } from 'react-native';
 import { Link } from 'expo-router';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 export default function HomeScreen() {
+
+  // const [fontsLoaded] = useFonts({
+  //   'CedarvilleCursive': require('./assets/fonts/CedarvilleCursive-Regular.ttf'),
+  // });
+
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/Poppins-Bold.ttf'),
+  });
+
+  if (!loaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Link style={styles.title} href={'/home'}>Home</Link>
-      <Link style={styles.title} href={'/loginModal'} >CLick Here to Login</Link>
-      <Link style={styles.title} href={'/signupModal'} >CLick Here to Sign Up</Link>
+      <Link style={[styles.title, styles.fontfam]} href={'/home'}>Mail Motion Your Marketing Partner</Link>
+      <Link style={[styles.title, styles.fontfam]} href={'/loginModal'}>Click Here to Login</Link>
+      <Link style={[styles.title, styles.fontfam]} href={'/signupModal'}>Click Here to Sign Up</Link>
     </View>
   );
 }
@@ -24,7 +38,11 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 30,
-    fontWeight: 800,
+    fontWeight: '800',
     color: 'white'
+  },
+  fontfam: {
+    fontFamily: 'Poppins-Bold',
+    // fontWeight: 800
   }
 });
