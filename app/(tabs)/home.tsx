03996f063c888b4900ccb73b8a303b8fbf9c92eb
hadi-razal <hadhirasal22@gmail.com>
2024-06-29 09:43:51 +0530

    updates in home page

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,68 +1,55 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
+import React from 'react';
+import { View, Text, TextInput, Pressable, Alert } from 'react-native';
 import { supabase } from '../../utils/supabase';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Colors } from '../../constants/Colors';
+
+interface Subscriber {
+  email: string;
+}
 
 export default function HomeScreen() {
-  const [email, setEmail] = useState('');
-  const [subscribers, setSubscribers] = useState([]);
-  const [emailsSent, setEmailsSent] = useState(10);
+  const [email, setEmail] = React.useState('');
 
-  const addSubscriber = async () => {
-    if (email) {
-      const newSubscribers = [...subscribers, email];
-      Alert.alert('Subscriber Added', `Email: ${email}`);
-
-      await supabase.from('subscribers').insert({ email })
-
-      setEmail('');
+  const handleSubmit = async () => {
+    const { error } = await supabase.from('subscribers').insert({ email });
+    if (error) {
+      Alert.alert('Subscription error', error.message);
     } else {
-      Alert.alert('Error', 'Please enter an email');
+      setEmail('');
+      Alert.alert('Success', 'Successfully subscribed!');
     }
   };
 
-  const renderSubscriber = ({ item }:any) => (
-    <View className="p-4 border-b border-gray-300">
-      <Text className="text-base text-gray-700">{item}</Text>
-    </View>
-  );
-
   return (
-    <View style={{ flexGrow: 1, backgroundColor: 'bg-gray-100' }}>
-      <View className="flex items-center pt-[120] p-4 ">
-        <Text className="text-3xl font-bold text-primaryColor mb-6">Welcome to Mail Motion</Text>
-        <View className="bg-primaryColor w-full items-center justify-center p-4 rounded-lg mb-8">
-          <Text className="text-xl font-semibold text-white mb-2">Emails Sent : {emailsSent}</Text>
-          <Text className="text-xl font-semibold text-white">Total Subscribers : {subscribers.length}</Text>
-        </View>
-        <View className="w-full mb-8">
-          <Text className="text-xl text-primaryColor mb-2 font-semibold">Add Subscriber</Text>
-          <View className="flex-row items-center">
-            <TextInput
-              className="flex-1 h-12 border border-gray-300 rounded-lg px-4 mr-4 bg-white"
-              placeholder="Enter email"
-              placeholderTextColor="#aaa"
-              value={email}
-              onChangeText={setEmail}
-            />
-            <TouchableOpacity
-              className="bg-primaryColor h-12 rounded-lg px-6 flex items-center justify-center"
-              onPress={addSubscriber}
-            >
-              <Text className="text-white font-semibold text-lg">Add</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View className="w-full">
-          <Text className="text-xl text-primaryColor mb-2 font-semibold">Subscribers List</Text>
-          <FlatList
-            data={subscribers}
-            renderItem={renderSubscriber}
-            keyExtractor={(item, index) => index.toString()}
-            className="bg-white rounded-lg p-4"
+    <View
+      className='flex flex-col justify-start min-h-screen pt-[80px]'
+    >
+      <View className='justify-center items-center p-5'>
+        <Text className='text-primaryColor text-center font-bold text-[48px]'>
+          Mail Motion
+        </Text>
+        <Text className='text-center text-gray-500 m-2'>
+          Keep your followers up-to-date with the latest news and offers!
+        </Text>
+        <View className='rounded-sm w-full max-w-md'>
+          <TextInput
+            value={email}
+            onChangeText={setEmail}
+            placeholder="Enter your email address"
+            placeholderTextColor="#ccc"
+            className='bg-white p-4 rounded-sm text-[15px] mb-4'
           />
+          <Pressable onPress={handleSubmit} className='bg-primaryColor p-4 rounded-sm'>
+            <Text className='text-white text-center font-bold'>Subscribe</Text>
+          </Pressable>
+        </View>
+        <View>
+          <Text className='text-center  font-extralight text-gray-500 mt-2'>
+            to add emails from a excel file then click on the plus
+          </Text>
         </View>
       </View>
     </View>
   );
 }
-
